05bd3f51908845cb831864c2802f15efa7dcd075
ihip <ivan.hip@gmail.com>
2019-10-14 22:30:10 +0200

    Verzija 1.0

---
+++ pitanja.js
@@ -0,0 +1,320 @@
+
+// klasa 0 - najlakša pitanja
+
+var pitanje0 = [
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko je korijen iz 625?",
+    upit2: "",
+    //  123456789012345678
+    a: "15",
+    b: "20",
+    c: "25",
+    d: "30",
+    tocno: 'c'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Suma kutova u trokutu je:",
+    upit2: "",
+    //  123456789012345678
+    a: "90°",
+    b: "120°",
+    c: "180°",
+    d: "360°",
+    tocno: 'c'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Što od nabrojanog nije valna pojava?",
+    upit2: "",
+    //  123456789012345678
+    a: "difrakcija",
+    b: "interferencija",
+    c: "refleksija",
+    d: "supstitucija",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Zakon akcije i reakcije je koji",
+    upit2: "Newtonov zakon:",
+    //  123456789012345678
+    a: "prvi",
+    b: "drugi",
+    c: "treći",
+    d: "četvrti",
+    tocno: 'c'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koji od ovih termina nije iz područja kemije?",
+    upit2: "",
+    //  123456789012345678
+    a: "akontacija",
+    b: "sublimacija",
+    c: "separacija",
+    d: "disocijacija",
+    tocno: 'a'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koji od nabrojanih elemenata nije metal?",
+    upit2: "",
+    //  123456789012345678
+    a: "litij",
+    b: "klor",
+    c: "volfram",
+    d: "berilij",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koji od ovih plinova nije plemeniti?",
+    upit2: "",
+    //  123456789012345678
+    a: "helij",
+    b: "neon",
+    c: "radon",
+    d: "ozon",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Broj molekula u jednom molu je:",
+    upit2: "",
+    //  123456789012345678
+    a: "Arhimedov broj",
+    b: "Arnoldov broj",
+    c: "Avicenin broj",
+    d: "Avogadrov broj",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koji od navedenih metala nije",
+    upit2: '"srebrne" boje:',
+    //  123456789012345678
+    a: "aluminij",
+    b: "bakar",
+    c: "cink",
+    d: "željezo",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko nogu ima osa?",
+    upit2: "",
+    //  123456789012345678
+    a: "četiri",
+    b: "šest",
+    c: "osam",
+    d: "deset",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko nogu ima pauk?",
+    upit2: "",
+    //  123456789012345678
+    a: "četiri",
+    b: "šest",
+    c: "osam",
+    d: "deset",
+    tocno: 'c'
+  }
+]
+
+
+// klasa 1 - laka pitanja
+
+var pitanje1 = [
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Ako su katete duljine 6 i 8 centimetara,",
+    upit2: "koliko centimetara je duljina hipotenuze?",
+    //  123456789012345678
+    a: "devet",
+    b: "deset",
+    c: "dvanaest",
+    d: "četrnaest",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko kvadratnih metara ima u jednom",
+    upit2: "kvadratnom kilometru?",
+    //  123456789012345678
+    a: "tisuću",
+    b: "milijun",
+    c: "milijarda",
+    d: "bilijun",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko kubičnih milimetara ima u jednom",
+    upit2: "kubičnom metru?",
+    //  123456789012345678
+    a: "tisuću",
+    b: "milijun",
+    c: "milijarda",
+    d: "bilijun",
+    tocno: 'c'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Toplina se mjeri u:",
+    upit2: "",
+    //  123456789012345678
+    a: "džulima",
+    b: "celzijima",
+    c: "kelvinima",
+    d: "farenhajtima",
+    tocno: 'a'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Kako se zvao prvi čovjek koji je stupio",
+    upit2: "na površinu Mjeseca?",
+    //  123456789012345678
+    a: "Lewis Hamilton",
+    b: "Neil Armstrong",
+    c: "Louis Armstrong",
+    d: "Neil Jordan",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Od koliko atoma se sastoji molekula metana?",
+    upit2: "",
+    //  123456789012345678
+    a: "dva",
+    b: "tri",
+    c: "četiri",
+    d: "pet",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Molekula amonijaka sastoji se od atoma:",
+    upit2: "",
+    //  123456789012345678
+    a: "dušika i vodika",
+    b: "dušika i kisika",
+    c: "antimona i ugljika",
+    d: "antimona i klora",
+    tocno: 'a'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "U kalcijevom karbonatu nema:",
+    upit2: "",
+    //  123456789012345678
+    a: "kalcija",
+    b: "vodika",
+    c: "kisika",
+    d: "ugljika",
+    tocno: 'b'
+  }
+]
+
+
+// klasa 2 - teža pitanja
+
+var pitanje2 = [
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Tko je autor popularnoznanstvene serije",
+    upit2: '"Kozmos" iz 1980. godine?',
+    //  123456789012345678
+    a: "Axl Rose",
+    b: "Bill Bryson",
+    c: "Carl Sagan",
+    d: "David Attenborough",
+    tocno: 'c'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Tko je izumio parni stroj?",
+    upit2: "",
+    //  123456789012345678
+    a: "Carnot",
+    b: "Joule",
+    c: "Kelvin",
+    d: "Watt",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Ako je duljina brida kocke tri metra, koliko",
+    upit2: "kvadratnih metara je oplošje?",
+    //  123456789012345678
+    a: "9",
+    b: "36",
+    c: "54",
+    d: "81",
+    tocno: 'c'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koji od ovih brojeva nije prim broj?",
+    upit2: "",
+    //  123456789012345678
+    a: "47",
+    b: "51",
+    c: "53",
+    d: "59",
+    tocno: 'b'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko nogu zajedno imaju mrav, pauk i pčela?",
+    upit2: "",
+    //  123456789012345678
+    a: "dvanaest",
+    b: "šesnaest",
+    c: "osamnaest",
+    d: "dvadeset",
+    tocno: 'd'
+  }
+]
+
+
+// klasa 3 - najteža pitanja
+
+var pitanje3 = [
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koliko atoma vodika sadrži molekula etanola?",
+    upit2: "",
+    //  123456789012345678
+    a: "jedan",
+    b: "četiri",
+    c: "pet",
+    d: "šest",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Koji od ovih brojeva je najbliži broju pi?",
+    upit2: "",
+    //  123456789012345678
+    a: "3,1415627",
+    b: "3,1415729",
+    c: "3,1415825",
+    d: "3,1415926",
+    tocno: 'd'
+  },
+  { 
+    //      12345678901234567890123456789012345678901234567
+    upit1: "Arhimed je živio na:",
+    upit2: "",
+    //  123456789012345678
+    a: "Kreti",
+    b: "Siciliji",
+    c: "Sardiniji",
+    d: "Rodosu",
+    tocno: 'b'
+  }
+]
