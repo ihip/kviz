068288faf1e06a60c04258a4bf3f6f4ab00bc9d3
ihip <ivan.hip@gmail.com>
2019-10-15 19:47:44 +0200

    V1.1 - nove fotke nagrada i više od 60 pitanja (uključujući Davorova i Jelenina)

--- pitanja.js
+++ pitanja.js
@@ -122,6 +122,105 @@ var pitanje0 = [
     c: "osam",
     d: "deset",
     tocno: 'c'
+  },
+  { 
+    // JL   12345678901234567890123456789012345678901234567
+    upit1: "Koliko iznosi jedna četvrtina od 5000?",
+    upit2: "",
+    //  123456789012345678
+    a: "1500",
+    b: "2500",
+    c: "1750",
+    d: "1250",
+    tocno: 'd'
+  },
+  { 
+    // JL   12345678901234567890123456789012345678901234567
+    upit1: "Koja se vrsta energije povezuje s gibanjem?",
+    upit2: "",
+    //  123456789012345678
+    a: "unutarnja",
+    b: "električna",
+    c: "kinetička",
+    d: "potencijalna",
+    tocno: 'c'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koliko je vremena potrebno staklu da se",
+    upit2: "razgradi?",
+    //  123456789012345678
+    a: "200 godina",
+    b: "500 godina",
+    c: "1000 godina",
+    d: "ne razgrađuje se",
+    tocno: 'd'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji je organizam među navedenima građen od",
+    upit2: "jedne stanice?",
+    //  123456789012345678
+    a: "hrast",
+    b: "papučica",
+    c: "miš",
+    d: "komarac",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji od navedenih je obnovljivi",
+    upit2: "izvor energije?",
+    //  123456789012345678
+    a: "nafta",
+    b: "nuklearna energija",
+    c: "voda",
+    d: "ugljen",
+    tocno: 'c'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji organ iz ljudskog tijela odstranjuje",
+    upit2: "nepotrebne i štetne tvari?",
+    //  123456789012345678
+    a: "srce",
+    b: "mozak",
+    c: "bubreg",
+    d: "kralježnica",
+    tocno: 'c'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji sastojak duhanskog dima uzrokuje",
+    upit2: "ovisnost u organizmu pušača?",
+    //  123456789012345678
+    a: "etilni alkohol",
+    b: "katran",
+    c: "ugljikov monoksid",
+    d: "nikotin",
+    tocno: 'd'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji od navedenih vjetrova puše na Jadranu?",
+    upit2: "",
+    //  123456789012345678
+    a: "polarni",
+    b: "bura",
+    c: "monsun",
+    d: "pasat",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Proces spajanja kemijskih elemenata s",
+    upit2: "kisikom zove se:",
+    //  123456789012345678
+    a: "sublimacija",
+    b: "destilacija",
+    c: "oksidacija",
+    d: "karbonizacija",
+    tocno: 'c'
   }
 ]
 
@@ -216,6 +315,83 @@ var pitanje1 = [
     c: "kisika",
     d: "ugljika",
     tocno: 'b'
+  },
+  { 
+    // JL   12345678901234567890123456789012345678901234567
+    upit1: "Kako se zove prva žena koja je dobila",
+    upit2: "Nobelovu nagradu za fiziku?",
+    //  123456789012345678
+    a: "Maria Goeppert-Mayer",
+    b: "Maria Curie",
+    c: "Irène Joliot-Curie",
+    d: "Bertha von Suttner",
+    tocno: 'b'
+  },
+  { 
+    // JL   12345678901234567890123456789012345678901234567
+    upit1: "Koji je kemijski element nakon kisika",
+    upit2: "najrašireniji u Zemljinoj kori?",
+    //  123456789012345678
+    a: "zlato",
+    b: "silicij",
+    c: "bakar",
+    d: "cink",
+    tocno: 'b'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: "Za koju računsku operaciju su potrebni",
+    upit2: "baza i eksponent?",
+    //  123456789012345678
+    a: "potenciranje",
+    b: "dijeljenje",
+    c: "množenje",
+    d: "logaritmiranje",
+    tocno: 'a'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji hormon se izlučuje kao reakcija",
+    upit2: "na stres?",
+    //  123456789012345678
+    a: "tiroksin",
+    b: "estrogen",
+    c: "progesteron",
+    d: "adrenalin",
+    tocno: 'd'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Kako drvenaste biljke upijaju vodu i",
+    upit2: "mineralne tvari:",
+    //  123456789012345678
+    a: "listovima",
+    b: "korijenom",
+    c: "stabljikom",
+    d: "cvjetovima",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Proces tijekom kojeg se morska voda pretvara",
+    upit2: "u slatku vodu naziva se:",
+    //  123456789012345678
+    a: "sublimacija",
+    b: "kondenzacija",
+    c: "desalinizacija",
+    d: "infiltracija",
+    tocno: 'c'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji organizam od navedenih je građen od više",
+    upit2: "stanica i stvara kisik za živa bića?",
+    //  123456789012345678
+    a: "vrganj",
+    b: "bor",
+    c: "papučica",
+    d: "miš",
+    tocno: 'b'
   }
 ]
 
@@ -277,6 +453,127 @@ var pitanje2 = [
     c: "osamnaest",
     d: "dvadeset",
     tocno: 'd'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: "Koja grana mehanike proučava skupove sila i",
+    upit2: "ravnotežu tijela na koja te sile djeluju?",
+    //  123456789012345678
+    a: "statika",
+    b: "dinamika",
+    c: "tehnička mehanika",
+    d: "kinematika",
+    tocno: 'a'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: "Avogadrov broj je 6,022 puta deset na koju",
+    upit2: "potenciju?",
+    //  123456789012345678
+    a: "21",
+    b: "22",
+    c: "23",
+    d: "24",
+    tocno: 'c'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: "Tko je uz bračni par Curie dobio Nobelovu",
+    upit2: "nagradu za otkriće radioaktivnosti?",
+    //  123456789012345678
+    a: "Albert Einstein",
+    b: "Henri Becquerel",
+    c: "Ernest Rutherford",
+    d: "Frederick Soddy",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Tanki površinski sloj na Zemlji kojim",
+    upit2: "neprestano kruži voda naziva se:",
+    //  123456789012345678
+    a: "atmosfera",
+    b: "troposfera",
+    c: "litosfera",
+    d: "hidrosfera",
+    tocno: 'd'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Porastom dubine mora dolazi do porasta:",
+    upit2: "",
+    //  123456789012345678
+    a: "tlaka",
+    b: "temperature",
+    c: "osvjetljenja",
+    d: "gustoće",
+    tocno: 'a'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koja od navedenih biljaka nije listopadna?",
+    upit2: "",
+    //  123456789012345678
+    a: "jasen",
+    b: "smreka",
+    c: "bukva",
+    d: "kesten",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Riječ ekologija dolazi od grčke riječi",
+    upit2: '"oikos" koja znači:',
+    //  123456789012345678
+    a: "otkos trave",
+    b: "dom, mjesto za život",
+    c: "raspjevani kos",
+    d: "živo biće",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Kako zovemo tvar koja se u otopini",
+    upit2: "istaloži na dno posude?",
+    //  123456789012345678
+    a: "rudiment",
+    b: "sediment",
+    c: "emulgator",
+    d: "filtrat",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "U koju kategoriju prostorne zaštite",
+    upit2: "pripadaju Kornati?",
+    //  123456789012345678
+    a: "nacionalni park",
+    b: "strogi rezervat",
+    c: "posebni rezervat",
+    d: "spomenik prirode",
+    tocno: 'a'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "U nizinskoj Hrvatskoj najviše hidroelektrana",
+    upit2: "izgrađeno je na rijeci:",
+    //  123456789012345678
+    a: "Muri",
+    b: "Dravi",
+    c: "Savi",
+    d: "Dunavu",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koji je među navedenim organizmima",
+    upit2: "polunametnik?",
+    //  123456789012345678
+    a: "imela",
+    b: "paprat",
+    c: "obični grab",
+    d: "lijeska",
+    tocno: 'a'
   }
 ]
 
@@ -316,5 +613,82 @@ var pitanje3 = [
     c: "Sardiniji",
     d: "Rodosu",
     tocno: 'b'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: 'Koji se stručni naziv za "višeobličje" koristi',
+    upit2: "u kemiji, biologiji i programiranju?",
+    //  123456789012345678
+    a: "polimorfizam",
+    b: "homomorfizam",
+    c: "multipleks",
+    d: "divergencija",
+    tocno: 'a'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: "Kako se zove zamišljena granica Zemljine",
+    upit2: "atmosfere i svemira?",
+    //  123456789012345678
+    a: "Armstrongova granica",
+    b: "troposfera",
+    c: "tropopauza",
+    d: "Kármánova granica",
+    tocno: 'd'
+  },
+  { 
+    // DS   12345678901234567890123456789012345678901234567
+    upit1: "Eulerov problem sa sedam mostova Königsberga",
+    upit2: "odnosi se na koji grad?",
+    //  123456789012345678
+    a: "Kalinjingrad",
+    b: "Vladivostok",
+    c: "Oslo",
+    d: "Beč",
+    tocno: 'a'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Na kovanici od 50 lipa prikazana je endemska",
+    upit2: "i reliktna vrsta:",
+    //  123456789012345678
+    a: "velebitska degenija",
+    b: "biokovsko zvonce",
+    c: "dubrovačka zečina",
+    d: "runolist",
+    tocno: 'a'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Koje su skupine ptica najugroženije",
+    upit2: "u Hrvatskoj?",
+    //  123456789012345678
+    a: "selice",
+    b: "močvarnice",
+    c: "stanarice",
+    d: "grabljivice",
+    tocno: 'b'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Najveće valove na Jadranu uzrokuje vjetar:",
+    upit2: "",
+    //  123456789012345678
+    a: "maestral",
+    b: "bura",
+    c: "jugo",
+    d: "tramontana",
+    tocno: 'c'
+  },
+  { 
+    // JL-O 12345678901234567890123456789012345678901234567
+    upit1: "Najviše slatke vode na Zemljinoj površini",
+    upit2: "ima u:",
+    //  123456789012345678
+    a: "rijekama",
+    b: "jezerima",
+    c: "podzemlju",
+    d: "ledenjacima",
+    tocno: 'd'
   }
 ]
